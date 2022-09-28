7bdb5c43f332fdd4149b21debf1411de4185873d
Di Az <114617446+dnlzrv@users.noreply.github.com>
2022-09-28 22:18:09 +0200

    Removed comments from Adjectives and Nouns python script.

--- python/2022.09.27_Practice_Ajectives_Nouns.py
+++ python/2022.09.27_Practice_Ajectives_Nouns.py
@@ -1,9 +1,5 @@
-adjectives = ["beste", "liebenswuerdigste", "schoenste", "groesste"]  # Bsp - kann beliebig erweitert werden
-nouns = ["Mensch", "Hecht", "Freund", "Kumpel", "Programmierer"]  # Bsp - kann beliebig erweitert werden
-
-# Bsp-Ausgabe für die Eingabe 2
-# Du bist der beste Freund
-# Du bist der groesste Kumpel
+adjectives = ["beste", "liebenswuerdigste", "schoenste", "groesste"]
+nouns = ["Mensch", "Hecht", "Freund", "Kumpel", "Programmierer"]
 
 numberOfCombinations = 0
 
